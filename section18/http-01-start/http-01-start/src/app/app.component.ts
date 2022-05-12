a49edf17a79bb3988105ac11b69f97f30d75abea
Hunterlemming <kklemming@gmail.com>
2022-05-12 12:05:31 +0200

    S17 - Using RxJS Operators to Transform Response Data

--- section18/http-01-start/http-01-start/src/app/app.component.ts
+++ section18/http-01-start/http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +8,47 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  //#region Properties
+  
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  //#endregion
 
-  ngOnInit() {}
+  //#region Variables
+
+  private baseUrl: string = 'https://ng-complete-guide-80289-default-rtdb.europe-west1.firebasedatabase.app/'
+
+  //#endregion
+
+constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(this.baseUrl + 'posts.json', postData)
+      .subscribe(response => console.log(response));
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(this.baseUrl + 'posts.json')
+      .pipe(
+        map(responseData => {
+          return Object.keys(responseData)
+            .map(key => ({...responseData[key], id: key}))
+        })
+      ).subscribe(posts => console.log(posts));
   }
 }
